50a9d05c0db2203d7e8bb0ee94958548644e65f7
DerFantastico <crazyherobrinehd@gmail.com>
2026-06-25 19:35:55 +0200

    Projektdateien hinzufügen.

---
+++ Texture Extender/TextureExtender.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Texture_Extender
+{
+    partial class frmTextureExtender
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnChooseImage = new Button();
+            tbxWidth = new TextBox();
+            tbxHeight = new TextBox();
+            lblWidth = new Label();
+            lblHeight = new Label();
+            btnSaveImage = new Button();
+            btnWidthPlus = new Button();
+            btnWidthMinus = new Button();
+            btnHeightMinus = new Button();
+            btnHeightPlus = new Button();
+            SuspendLayout();
+            // 
+            // btnChooseImage
+            // 
+            btnChooseImage.Location = new Point(12, 12);
+            btnChooseImage.Name = "btnChooseImage";
+            btnChooseImage.Size = new Size(123, 33);
+            btnChooseImage.TabIndex = 0;
+            btnChooseImage.Text = "Choose Image";
+            btnChooseImage.UseVisualStyleBackColor = true;
+            btnChooseImage.Click += btnChooseImage_Click;
+            // 
+            // tbxWidth
+            // 
+            tbxWidth.Location = new Point(12, 82);
+            tbxWidth.Name = "tbxWidth";
+            tbxWidth.Size = new Size(123, 23);
+            tbxWidth.TabIndex = 1;
+            tbxWidth.Text = "1";
+            // 
+            // tbxHeight
+            // 
+            tbxHeight.Location = new Point(12, 141);
+            tbxHeight.Name = "tbxHeight";
+            tbxHeight.Size = new Size(123, 23);
+            tbxHeight.TabIndex = 2;
+            tbxHeight.Text = "1";
+            // 
+            // lblWidth
+            // 
+            lblWidth.AutoSize = true;
+            lblWidth.Location = new Point(12, 64);
+            lblWidth.Name = "lblWidth";
+            lblWidth.Size = new Size(39, 15);
+            lblWidth.TabIndex = 3;
+            lblWidth.Text = "Width";
+            // 
+            // lblHeight
+            // 
+            lblHeight.AutoSize = true;
+            lblHeight.Location = new Point(12, 123);
+            lblHeight.Name = "lblHeight";
+            lblHeight.Size = new Size(43, 15);
+            lblHeight.TabIndex = 4;
+            lblHeight.Text = "Height";
+            // 
+            // btnSaveImage
+            // 
+            btnSaveImage.Location = new Point(12, 191);
+            btnSaveImage.Name = "btnSaveImage";
+            btnSaveImage.Size = new Size(123, 33);
+            btnSaveImage.TabIndex = 5;
+            btnSaveImage.Text = "Save Iamge";
+            btnSaveImage.UseVisualStyleBackColor = true;
+            btnSaveImage.Click += btnSaveImage_Click;
+            // 
+            // btnWidthPlus
+            // 
+            btnWidthPlus.Location = new Point(141, 82);
+            btnWidthPlus.Name = "btnWidthPlus";
+            btnWidthPlus.Size = new Size(23, 23);
+            btnWidthPlus.TabIndex = 6;
+            btnWidthPlus.Text = "+";
+            btnWidthPlus.UseVisualStyleBackColor = true;
+            btnWidthPlus.Click += btnWidthPlus_Click;
+            // 
+            // btnWidthMinus
+            // 
+            btnWidthMinus.Location = new Point(170, 82);
+            btnWidthMinus.Name = "btnWidthMinus";
+            btnWidthMinus.Size = new Size(23, 23);
+            btnWidthMinus.TabIndex = 7;
+            btnWidthMinus.Text = "-";
+            btnWidthMinus.UseVisualStyleBackColor = true;
+            btnWidthMinus.Click += btnWidthMinus_Click;
+            // 
+            // btnHeightMinus
+            // 
+            btnHeightMinus.Location = new Point(170, 141);
+            btnHeightMinus.Name = "btnHeightMinus";
+            btnHeightMinus.Size = new Size(23, 23);
+            btnHeightMinus.TabIndex = 9;
+            btnHeightMinus.Text = "-";
+            btnHeightMinus.UseVisualStyleBackColor = true;
+            btnHeightMinus.Click += btnHeightMinus_Click;
+            // 
+            // btnHeightPlus
+            // 
+            btnHeightPlus.Location = new Point(141, 141);
+            btnHeightPlus.Name = "btnHeightPlus";
+            btnHeightPlus.Size = new Size(23, 23);
+            btnHeightPlus.TabIndex = 8;
+            btnHeightPlus.Text = "+";
+            btnHeightPlus.UseVisualStyleBackColor = true;
+            btnHeightPlus.Click += btnHeightPlus_Click;
+            // 
+            // frmTextureExtender
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnHeightMinus);
+            Controls.Add(btnHeightPlus);
+            Controls.Add(btnWidthMinus);
+            Controls.Add(btnWidthPlus);
+            Controls.Add(btnSaveImage);
+            Controls.Add(lblHeight);
+            Controls.Add(lblWidth);
+            Controls.Add(tbxHeight);
+            Controls.Add(tbxWidth);
+            Controls.Add(btnChooseImage);
+            Name = "frmTextureExtender";
+            Text = "Texture Extender";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnChooseImage;
+        private TextBox tbxWidth;
+        private TextBox tbxHeight;
+        private Label lblWidth;
+        private Label lblHeight;
+        private Button btnSaveImage;
+        private Button btnWidthPlus;
+        private Button btnWidthMinus;
+        private Button btnHeightMinus;
+        private Button btnHeightPlus;
+    }
+}
---
+++ Texture Extender/TextureExtender.cs
@@ -0,0 +1,137 @@
+using System.Drawing.Imaging;
+using System.Text.RegularExpressions;
+
+namespace Texture_Extender
+{
+    public partial class frmTextureExtender : Form
+    {
+        string image;
+        string extension;
+        string imageName;
+
+        public frmTextureExtender()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWidthPlus_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(tbxWidth.Text, "[0-9]"))
+            {
+                tbxWidth.Text = Convert.ToString(Convert.ToInt32(tbxWidth.Text) + 1);
+            }
+            else
+            {
+                tbxWidth.Text = "1";
+            }
+        }
+
+        private void btnWidthMinus_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(tbxWidth.Text, "[0-9]"))
+            {
+                if (Convert.ToInt32(tbxWidth.Text) > 1)
+                {
+                    tbxWidth.Text = Convert.ToString(Convert.ToInt32(tbxWidth.Text) - 1);
+                }
+            }
+            else
+            {
+                tbxWidth.Text = "1";
+            }
+        }
+
+        private void btnHeightPlus_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(tbxHeight.Text, "[0-9]"))
+            {
+                tbxHeight.Text = Convert.ToString(Convert.ToInt32(tbxHeight.Text) + 1);
+            }
+            else
+            {
+                tbxHeight.Text = "1";
+            }
+        }
+
+        private void btnHeightMinus_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(tbxHeight.Text, "[0-9]"))
+            {
+                if (Convert.ToInt32(tbxHeight.Text) > 1)
+                {
+                    tbxHeight.Text = Convert.ToString(Convert.ToInt32(tbxHeight.Text) - 1);
+                }
+            }
+            else
+            {
+                tbxHeight.Text = "1";
+            }
+        }
+
+        private void btnChooseImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Choose Image";
+            ofd.Filter = "Image Files|*.png;*.jpg";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                image = ofd.FileName;
+                extension = Path.GetExtension(ofd.FileName);
+                imageName = Path.GetFileNameWithoutExtension(ofd.FileName);
+            }
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(image))
+            {
+                MessageBox.Show("No image selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!Regex.IsMatch(tbxWidth.Text,"[0-9]") || !Regex.IsMatch(tbxHeight.Text, "[0-9]"))
+            {
+                MessageBox.Show("Width or height is not a number!","Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Save Image";
+            sfd.Filter = "Image Files|*." + extension;
+
+            if(sfd.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap bmp = ImageExtender();
+                if(extension == ".png")
+                {
+                    bmp.Save(sfd.FileName, ImageFormat.Png);
+                }
+                else
+                {
+                    bmp.Save(sfd.FileName, ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private Bitmap ImageExtender()
+        {
+            int w = Convert.ToInt32(tbxWidth.Text);
+            int h = Convert.ToInt32(tbxHeight.Text);
+
+            Bitmap source = new Bitmap(image);
+            Bitmap extended = new Bitmap(source.Width*w, source.Height*h);
+
+            Graphics g = Graphics.FromImage(extended);
+
+            for(int i = 0; i < h; i++)
+            {
+                for(int j = 0; j < w; j++)
+                {
+                    g.DrawImage(source, i*source.Width, j*source.Height);
+                }
+            }
+
+            return extended;
+        }
+    }
+}
